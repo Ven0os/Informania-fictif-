cf3c758d62d6a0329d6b6e53f86f736d17b54338
Leroy Bryan <91132339+Az-Back@users.noreply.github.com>
2021-10-19 15:46:41 +0200

    Add files via upload

---
+++ Styles/Marketing.js
@@ -0,0 +1,10 @@
+var myImage = document.getElementById('mainImage');
+image1.onclick = function(){
+	myImage.setAttribute("src", "../Images/Flyer.png");
+}
+image2.onclick = function(){
+	myImage.setAttribute("src", "../Images/Informania.png");
+}
+image3.onclick = function(){
+	myImage.setAttribute("src", "../Images/Logo1.png");
+}
